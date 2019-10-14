b3c927bf549a95c0535647dc7bad4c8a8f9d99f8
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2019-10-14 12:40:51 +1100

    implement in-map combiner

--- mapreduce/src/com/samuelsen/cc/WordMapper.java
+++ mapreduce/src/com/samuelsen/cc/WordMapper.java
@@ -5,20 +5,38 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
 
+    private final Map<String, Integer> wordMap = new HashMap<>();
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-        StringTokenizer itr = new StringTokenizer(value.toString());
-        while (itr.hasMoreTokens()) {
-            word.set(itr.nextToken());
-            context.write(word, one);
+        StringTokenizer tokenizer = new StringTokenizer(value.toString());
+        String token = "";
+        while (tokenizer.hasMoreTokens()) {
+            token = tokenizer.nextToken();
+            if (wordMap.containsKey(token)) wordMap.put(token,wordMap.get(token) + 1);
+            else wordMap.put(token, 1);
+        }
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        super.cleanup(context);
+        wordMap.forEach((key, value) -> write(key, value, context));
+    }
+
+    private static void write(String key, Integer value, Context context)  {
+        System.out.println("Key: " + key + ", value: " + value);
+        try {
+            context.write(new Text(key), new IntWritable(value));
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
